5161a884d1068f988cf2c2fdb1d57fb2788cc262
wilo1514 <williamlituma072@gmail.com>
2026-04-16 08:33:22 -0500

    notificaciones actualizadas

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-// Usamos las variables de entorno de Vite. 
-// Si no existe, usamos localhost por defecto.
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:7018';
+
+//const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:7018';
+const API_URL = import.meta.env.VITE_API_URL || 'http://10.10.2.122:8085';
+
 
 const api = axios.create({
   baseURL: API_URL,
